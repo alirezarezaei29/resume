ce14b31bd52368b1994c2b490e36388d3891e3a0
Alireza <cavalier.boy76@gmail.com>
2020-03-19 02:19:25 +0330

    Updates

---
+++ precache-manifest.90d269ab5ed659bff71f28a3bc0b4e77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c40ecb98350afa1cb93602b508bf878",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "aab99391036f0b404481",
+    "url": "/resume/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "eaec128f558bcbde956a",
+    "url": "/resume/static/css/main.52baa9ac.chunk.css"
+  },
+  {
+    "revision": "aab99391036f0b404481",
+    "url": "/resume/static/js/2.cdecc39e.chunk.js"
+  },
+  {
+    "revision": "9daad20423a52da1b13ee6b55467f561",
+    "url": "/resume/static/js/2.cdecc39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaec128f558bcbde956a",
+    "url": "/resume/static/js/main.58761ab8.chunk.js"
+  },
+  {
+    "revision": "540de6763af4833a1da8",
+    "url": "/resume/static/js/runtime-main.64f02ed5.js"
+  },
+  {
+    "revision": "85ac02da5514bcf00db55facd21f26e3",
+    "url": "/resume/static/media/Iceland-Regular.85ac02da.ttf"
+  }
+]);
